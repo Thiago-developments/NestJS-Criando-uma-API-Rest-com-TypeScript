4c849b8365e247ca0eb785436061ef35019fc385
Thiago Sousa <thiagosvin@hotmail.com>
2021-10-17 19:29:49 -0300

    Integrando com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './product.model';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'products',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Product])
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
--- src/products.service.ts
+++ src/products.service.ts
@@ -4,9 +4,9 @@ import { Product } from "./product.model";
 @Injectable()
 export class ProductsService {
     products: Product[] = [
-        new Product("BOOK01", "BDD AND TDD Book on practice", 29.90),
-        new Product("BOOK02", "Flutter for beginners", 39.90),
-        new Product("BOOK03", "IA as a service ", 19.90),
+        // new Product("BOOK01", "BDD AND TDD Book on practice", 29.90),
+        // new Product("BOOK02", "Flutter for beginners", 39.90),
+        // new Product("BOOK03", "IA as a service ", 19.90),
     ];
 
     getAll(): Product[] {
