3e44931a5482b58c33c4ab0bb6faaf7f6e1cb752
Thiago Sousa <thiagosvin@hotmail.com>
2021-10-17 04:08:40 -0300

    Models

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,30 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Controller('products')
 export class ProductsController {
+    products: Product[] = [
+        new Product("BOOK01", "BDD AND TDD Book on practice", 29.90),
+        new Product("BOOK02", "Flutter for beginners", 39.90),
+        new Product("BOOK03", "IA as a service ", 19.90),
+    ];
     @Get()
-    getAll():string{
-        return 'All products list';
+    getAll():Product[]{
+        return this.products;
     }
 
     @Get(':id')
-    getOne(@Param() params): string {
-        return `Return data from product ${params.id}`
+    getOne(@Param() params): Product {
+        return this.products[0];
     }
 
     @Post()
-    create(@Body() product): string{
-        console.log(product);
-        return 'Product created';
+    create(@Body() product : Product) {
+       product.id = 100;
+        this.products.push(product);
     }
 
     @Put()
-    update(@Body() product): string{
-        console.log(product);
-        return 'Product updated';
+    update(@Body() product: Product): Product{
+        return product;
     }
     @Delete(':id')
-    delete(@Param() params):string {
-        return `Delete ${params.id}º product`
+    delete(@Param() params) {
+        this.products.pop();
     }
 }
